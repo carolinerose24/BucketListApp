b5121e50d2c55d14a6fd5f6cc053c092db77f1cf
Caroline Jarman <carolinerosejarman@gmail.com>
2018-11-30 08:57:13 -0700

    Added code to bucket item cell

--- BucketListApp/Controller/BucketItemCellTableViewCell.swift
+++ BucketListApp/Controller/BucketItemCellTableViewCell.swift
@@ -9,13 +9,57 @@
 import UIKit
 
 class BucketItemCellTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
+    @IBOutlet weak var bucketItemSymbol: UILabel!
+    @IBOutlet weak var bucketItemText: UILabel!
+    @IBOutlet weak var bucketItemSignature: UILabel!
+    
+    var currentBucketItem : BucketItem!
+    {
+      didSet
+      {
+            updateCellView()
+      }
+    }
+    
+    private func randomEmoji() -> String
+    {
+        let emojiStart = 0x1f601
+        let emojiEnd = 0x1f64F
+        let symbolStart = 0x1f680
+        let symbolEnd = 0x1f6C5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
+    }
+    
+    private func updateCellView() -> Void
+    {
+        if (currentBucketItem != nil)
+        {
+            bucketItemSignature.text = currentBucketItem.itemAuthor
+            bucketItemText.text = currentBucketItem.itemContents
+        }
+        else
+        {
+            bucketItemSignature.text = "Author goes here"
+            bucketItemText.text = "Bucket item contents here"
+        }
+        bucketItemSymbol.text = randomEmoji()
+    }
+    
+    
+    override func awakeFromNib()
+    {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
